5bb1823c13a65298c0aba8713641627dea4d7e09
Rahmeen-R <rahmeenrajput27@gmail.com>
2026-04-28 14:52:29 +0100

    Update Python AI Assistant.py

--- Python AI Assistant.py
+++ Python AI Assistant.py
@@ -7,8 +7,7 @@ load_dotenv()
 # Initialize LLM
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    temperature=0.7,
-    api_key="AIzaSyC-g_GisXRzDuArgodrkDHzgt50UlQF7uY"
+    temperature=0.7
 )
 
 # Page Configuration
